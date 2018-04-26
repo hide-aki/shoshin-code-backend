fb71d09f23a2b59ec0fec913cf5f1e0810959651
jaf7 <janthonyfields@gmail.com>
2018-04-26 18:52:06 -0400

    Added root route to config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :exercises, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/', to: 'exercises#index'
+  # get '/', to: 'exercises#index'
+  root :to => 'exercises#index'
 end 
