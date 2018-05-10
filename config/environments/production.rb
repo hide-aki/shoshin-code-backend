c297c130fa74482406db81b1a63bb87c1b4b8486
jaf7 <janthonyfields@gmail.com>
2018-05-09 21:24:23 -0400

    change allowed request origins

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,8 +36,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.allowed_request_origins = [ 'https://shoshin-code-frontend.herokuapp.com', /https:\/\/shoshin-code-frontend.*/ ]
-  # config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
+  # config.action_cable.allowed_request_origins = [ 'https://shoshin-code-frontend.herokuapp.com', /https:\/\/shoshin-code-frontend.*/ ]
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
